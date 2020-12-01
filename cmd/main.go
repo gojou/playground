e4871fb1579c7a1578748c6432443c82cc64b0fa
mark <mark.poling@gmail.com>
2020-12-01 04:11:55 -0500

    Getting pedantic

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Printf("%v\n", "Hello World!")
+	fmt.Printf("%v\n", "Hello guys!")
 	var persons []person.Person
 
 	persons = append(persons, person.Person{Firstname: "Mark", Lastname: "Poling"})
 	persons = append(persons, person.Person{Firstname: "Liz", Lastname: "Poling-Hiraldo"})
 	persons = append(persons, person.Person{Firstname: "Aden", Lastname: "Poling"})
-	persons = append(persons, person.Person{Lastname: "Poling", Firstname: "Rhi"})
+	persons = append(persons, person.Person{Firstname: "Rhi", Lastname: "Poling"})
 
 	for _, p := range persons {
 		fmt.Printf("%v\n", p.GetLastFirst())
