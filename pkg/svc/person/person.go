d16d6c0d3f6e0b6e85d1db6974fb5385396766b6
mark <mark.poling@gmail.com>
2020-12-01 04:34:31 -0500

    added bson, json notation to Person struct

--- pkg/svc/person/person.go
+++ pkg/svc/person/person.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Person is potential Soylent Green
 type Person struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Firstname string             `bson:"firstname,omitempty"`
-	Lastname  string             `bson:"lastname,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Firstname string             `bson:"firstname,omitempty" json:"firstname,omitempty"`
+	Lastname  string             `bson:"lastname,omitempty" json:"lastname,omitempty"`
 }
