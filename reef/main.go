089ff596387d72cec139efb189f1742fe284f671
mark <mark.poling@gmail.com>
2020-12-23 18:59:03 -0500

    separated cases into cases.go for brevity in main.go

--- reef/main.go
+++ reef/main.go
@@ -1,3 +1,8 @@
+// This is modified from the lesson found at:
+//		https://www.youtube.com/watch?v=oiX7fAmOYX0
+// Let's gets started with Go Reflection, https://golang.org/pkg/reflect/
+// Source code: https://github.com/striversity/go-on-the-run
+
 package main
 
 import "fmt"
@@ -29,42 +34,7 @@ func main() {
 	// hoo = 13 // COMPILE ERROR -- hoo initialized with concrete type string, must be string
 	// fmt.Println(hoo)
 
-	// myBool := true
-	// myInt := 2010
-	// myFloat64 := 9.15
-	// myString := "Hello World!"
-	// myComplex128 := (7 + 7i)
-	// myIdentity := identity("br549")
-	// myIdentityCast := string(myIdentity)
-	// myBytesSized := [5]byte{}
-	// myBytesUnsized := []byte{}
-	// myMap := map[string]int{"a": 1, "b": 2, "c": 3}
-	// myPersonEmpty := *new(person)
-	// myPerson := person{firstName: "Jayne", lastName: "Doe", age: 34}
-	// myPersonPointer := &myPerson
-	// myChan := make(chan int)
-	// myNil := interface{}(nil)
-
-	var mySlice []interface{}
-	mySlice = append(mySlice,
-		true,                      // bool
-		2010,                      // int
-		98.6,                      // float64
-		"Hey there!",              // string
-		(7 + 7i),                  // complex128
-		identity("br549"),         // main.identity
-		string(identity("br549")), // main.identity cast to string
-		[5]byte{},                 // slice of bytes, sized to 5
-		[]byte{},                  // slice of bytes, unsized
-		map[string]int{"a": 1, "b": 2, "c": 3},
-		*new(person),
-		person{firstName: "Jayne", lastName: "Doe", age: 34},
-		&person{firstName: "Jayne", lastName: "Doe", age: 34},
-		make(chan int),
-		nil,
-	)
-
-	for _, elem := range mySlice {
+	for _, elem := range getCases() {
 		fmt.Printf("type is: %T -- value is: %v\n", elem, elem)
 	}
 }
