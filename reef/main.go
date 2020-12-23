8ddf7b27f29018af26e20ba5da4dec9b372ebf8a
mark <mark.poling@gmail.com>
2020-12-23 17:49:14 -0500

    basic type expamples refined a bit more

--- reef/main.go
+++ reef/main.go
@@ -29,26 +29,40 @@ func main() {
 	// hoo = 13 // COMPILE ERROR -- hoo initialized with concrete type string, must be string
 	// fmt.Println(hoo)
 
-	myBool := true
-	myInt := 2010
-	myFloat64 := 9.15
-	myString := "Hello World!"
-	myComplex128 := (7 + 7i)
-	myIdentity := identity("br549")
-	myIdentityCast := string(myIdentity)
-	myBytesSized := [5]byte{}
-	myBytesUnsized := []byte{}
-	myMap := map[string]int{"a": 1, "b": 2, "c": 3}
-	myPersonEmpty := *new(person)
-	myPerson := person{firstName: "Jayne", lastName: "Doe", age: 34}
-	myPersonPointer := &myPerson
-	myChan := make(chan int)
-	myNil := interface{}(nil)
+	// myBool := true
+	// myInt := 2010
+	// myFloat64 := 9.15
+	// myString := "Hello World!"
+	// myComplex128 := (7 + 7i)
+	// myIdentity := identity("br549")
+	// myIdentityCast := string(myIdentity)
+	// myBytesSized := [5]byte{}
+	// myBytesUnsized := []byte{}
+	// myMap := map[string]int{"a": 1, "b": 2, "c": 3}
+	// myPersonEmpty := *new(person)
+	// myPerson := person{firstName: "Jayne", lastName: "Doe", age: 34}
+	// myPersonPointer := &myPerson
+	// myChan := make(chan int)
+	// myNil := interface{}(nil)
 
 	var mySlice []interface{}
-	mySlice = append(mySlice, myBool, myInt, myFloat64, myString,
-		myComplex128, myIdentity, myIdentityCast, myBytesSized, myBytesUnsized,
-		myMap, myPersonEmpty, myPerson, myPersonPointer, myChan, myNil)
+	mySlice = append(mySlice,
+		true,                      // bool
+		2010,                      // int
+		98.6,                      // float64
+		"Hey there!",              // string
+		(7 + 7i),                  // complex128
+		identity("br549"),         // main.identity
+		string(identity("br549")), // main.identity cast to string
+		[5]byte{},                 // slice of bytes, sized to 5
+		[]byte{},                  // slice of bytes, unsized
+		map[string]int{"a": 1, "b": 2, "c": 3},
+		*new(person),
+		person{firstName: "Jayne", lastName: "Doe", age: 34},
+		&person{firstName: "Jayne", lastName: "Doe", age: 34},
+		make(chan int),
+		nil,
+	)
 
 	for _, elem := range mySlice {
 		fmt.Printf("type is: %T -- value is: %v\n", elem, elem)
